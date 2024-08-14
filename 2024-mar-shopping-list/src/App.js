594e6ada953fa1c7f92e6e402e8504388625d806
hickmanp60@gmail.com <you@example.com>
2024-08-13 21:46:34 -0400

    react app

---
+++ 2024-mar-shopping-list/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Shopping List</h1>
+      </header>
+
+      <main></main>
+    </div>
+  );
+}
