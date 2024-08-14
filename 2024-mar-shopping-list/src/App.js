d960855fa899ec05524c2df88d68d39f2c054aab
hickmanp60@gmail.com <you@example.com>
2024-08-14 16:13:50 -0400

    fetch

--- 2024-mar-shopping-list/src/App.js
+++ 2024-mar-shopping-list/src/App.js
@@ -1,13 +1,32 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
+import ShoppingForm from './Components/ShoppingForm/ShoppingForm';
+import ShoppingList from './Components/ShoppingList/ShoppingList';
 
 export default function App() {
+  const [shoppingList, setShoppingList] = useState([]);
+
+  const loadData = () => {
+    fetch("https://vv2mdj-8081.csb.app/api/list")
+      .then((x) => x.json())
+      .then((response) => {
+        setShoppingList(response);
+      });
+  };
+  
+  useEffect(loadData, []);
+  
   return (
     <div className="App">
       <header className="App-header">
         <h1>Shopping List</h1>
       </header>
 
-      <main></main>
+      <main>
+        <ShoppingForm />
+        <ShoppingList items={shoppingList} />
+      </main>
     </div>
   );
 }
--- 2024-mar-shopping-list/src/Components/ShoppingList/ShoppingList.jsx
+++ 2024-mar-shopping-list/src/Components/ShoppingList/ShoppingList.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
 
-export default function ShoppingList() {
-    return(
-        <div>ShoppingList</div>
+function ShoppingItem(props) {
+    return <li>{props.item} ({props.quantity}) <button>Delete</button></li>;
+}
+
+export default function ShoppingList({ items }) {
+console.log(items)
+    const ItemsJsx = items.map(listItem =>
+            <ShoppingItem
+                key={listItem.id}
+                id={listItem.id}
+                item={listItem.item}
+                quantity={listItem.quantity}
+            />
     )
+
+    return(
+        <ul>{ItemsJsx}</ul>
+    );
 }
